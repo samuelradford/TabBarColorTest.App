9316753420f2e6ca83c2034809e4e9656d9dec2d
Sam Catchpole-Radford <s.catchpoleradford@theaccessgroup.com>
2023-01-03 09:55:24 +0000

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TabBarColorTest;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TabBarColorTest;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
